fcfa338c26724631d0006efe6c1113a1593dfb6f
Jehyun <congmul@hotmail.com>
2023-03-06 21:52:33 -0800

    local

---
+++ src/components/Pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Nav from '../../components/Nav'
+import Footer from '../../components/Footer'
+import AboutBody from '../../components/AboutBody'
+
+export default function About() {
+  return (
+    <>
+    <div className="empty-space"></div>
+    <div className="bar">
+      <Nav />
+    </div>
+    <div className="project-box-about">
+        
+        <AboutBody />
+    
+    </div>
+    <Footer />
+    
+    </>
+
+  );
+} 
+
